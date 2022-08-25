a67136539fbe02732d4f2bda4b6f480ee4cc9182
amortenson <amortenson@campbellsci.com>
2022-08-24 20:20:26 -0600

    feat: Added all the button handlers.

--- app/index.js
+++ app/index.js
@@ -3,12 +3,24 @@ console.log('Hello world!');
 import * as document from "document";
 import { FileIOFitbit } from "./FileIO";
 
-// Btn Handler
+// Btn Handlers
 const btnLeft = document.getElementById("btn-left");
-
 btnLeft.addEventListener("click", (evt) => {
   console.log("Left CLICKED");
 });
+const btnRight = document.getElementById("btn-right");
+btnRight.addEventListener("click", (evt) => {
+  console.log("Right CLICKED");
+});
+const btnHistory = document.getElementById("btn-history");
+btnHistory.addEventListener("click", (evt) => {
+  console.log("History CLICKED");
+});
+const btnRtn = document.getElementById("history-screen__rtn-btn");
+btnRtn.addEventListener("click", (evt) => {
+  console.log("Return CLICKED");
+});
+
 
 const fileIO = new FileIOFitbit({ encoding: 'json' });
 
