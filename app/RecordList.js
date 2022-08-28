ca0f9b7fdcb40af074f0071a29bb342b5307a141
amortenson <amortenson@campbellsci.com>
2022-08-27 20:43:14 -0600

    feat: Added an object for managing a list of records.

---
+++ app/RecordList.js
@@ -0,0 +1,37 @@
+import { DateToJson } from "./DateHelpers";
+
+export class RecordList{
+    /**
+     * @param {object} options Specifies the options. 
+     * @param {FileIoInterface} options.fileIOInterface Specifies a fileIO interface that reads and writes
+     * js objects.
+     * @param {function(object[])} options.onRecord Specifies a function that is called when a 
+     * record is added. The function called will get an updated list of records.
+     * @param {number} options.recordMax Specifies the maximun records in the records list.
+     */
+    constructor({fileIoInterface, onRecord, recordMax = 10}){
+        this.onRecord = onRecord;
+        this.max = recordMax;
+        this.io = fileIoInterface;
+        this.records = this.io.read();
+    }
+
+    /**
+     * Adds a record to the record list.
+     * @param {str} side Specifies the side of the event.
+     * @param {date} date Specifies the date of the event.
+     */
+    addRecord(side, date){
+        // add record to record
+        const record = {side, date: DateToJson(date)};
+        this.records.unshift(record);
+        // remove any extra records
+        while(this.records.length > this.max){
+            this.records.pop();
+        }
+        // Alert the UI.
+        this.onRecord(this.records);
+        // Reset the store
+        this.io.write(this.records);
+    } 
+}
